85dc8c4431c42318c221824a17f04e85a9508705
Style Mistake <stylemistake@gmail.com>
2014-03-03 13:17:43 +0200

    Multiple changes [add] Added LED feedback for pads with no callbacks [add] Added track activator/solo/arm button support [chg] Removed myself from the vendor field [chg] Some refactoring work [chg] Moved event system to the separate class [fix] Fixed auto-detection on Linux [fix] Fixed bugs with clip launcher led feedback [fix] Fixed clip launcher indicators not disappearing after script exits

--- apc40.control.js
+++ apc40.control.js
@@ -4,12 +4,9 @@ load("lib/polyfills.js");
 load("lib/helpers.js");
 load("lib/apc40.js");
 
-
-host.defineController(
-	"stylemistake", "Akai APC40",
-	"0.1", "0894C4A0-998E-11E3-A5E2-0800200C9A66"
-);
+host.defineController( "Akai", "APC40", "0.1", "0894C4A0-998E-11E3-A5E2-0800200C9A66" );
 host.defineMidiPorts( 1, 1 );
+host.addDeviceNameBasedDiscoveryPair( ["Akai APC40 MIDI 1"], ["Akai APC40 MIDI 1"] );
 host.addDeviceNameBasedDiscoveryPair( ["Akai APC40"], ["Akai APC40"] );
 
 var controller, transport, userctl, tracks, master, application;
@@ -96,18 +93,50 @@ function init() {
 	//  Track/Master volume mappings
 	// -------------------------------------------------------------------
 
-	controller.setEventCallback( "track_volume_change", function( data ) {
+	controller.setEventCallback( "track_volume_change", function( track, value ) {
 		// TODO: Master track volume must be modifiable only by master fader
 		//       Also, they should not scroll together with clip view.
-		tracks.getTrack( data.track ).getVolume().set( data.value, 188 );
-
-		// Can be assigned to anything you like (pew pew fx ftw).
-		// userctl.getControl( 20 + data.track ).inc( data.value, 128 );
+		tracks.getTrack( track ).getVolume().set( value, 188 );
 	});
 
 	controller.setEventCallback( "master_volume_change", function( value ) {
 		master.getVolume().set( value, 188 );
 	});
+
+
+	// -------------------------------------------------------------------
+	//  Track toggles
+	// -------------------------------------------------------------------
+
+	controller.setEventCallback( "track_activator_press", function( track ) {
+		tracks.getTrack( track ).getMute().toggle();
+	});
+
+	for ( var i = 0; i < 8; i += 1 ) (function( i, track ) {
+		track.getMute().addValueObserver( function( is_muted ) {
+			controller.setActivatorLed( i, is_muted ? 0 : 1 );
+		});
+	})( i, tracks.getTrack(i) );
+
+	controller.setEventCallback( "track_solo_press", function( track ) {
+		tracks.getTrack( track ).getSolo().toggle();
+	});
+
+	for ( var i = 0; i < 8; i += 1 ) (function( i, track ) {
+		track.getSolo().addValueObserver( function( is_solo ) {
+			controller.setSoloLed( i, is_solo ? 1 : 0 );
+		});
+	})( i, tracks.getTrack(i) );
+
+	controller.setEventCallback( "track_arm_press", function( track ) {
+		tracks.getTrack( track ).getArm().toggle();
+	});
+
+	for ( var i = 0; i < 8; i += 1 ) (function( i, track ) {
+		track.getArm().addValueObserver( function( is_armed ) {
+			controller.setArmLed( i, is_armed ? 1 : 0 );
+		});
+	})( i, tracks.getTrack(i) );
 
 
 	// -------------------------------------------------------------------
@@ -144,26 +173,28 @@ function init() {
 
 	for ( var i = 0; i < 8; i += 1 ) (function( i, slots ) {
 		slots.setIndication( true );
-		var _led = function( slot, color ) {
-			return { "scope": "clip", "x": i, "y": slot, "value": color };
+		var states = [{},{},{},{},{},{},{},{}];
+		var _led = function( slot, type, status ) {
+			states[ slot ][ type ] = status;
+			var state = states[ slot ];
+			var result = { "scope": "clip", "x": i, "y": slot, "value": 0 };
+			if ( state.content ) result.value = 5;
+			if ( state.playing ) result.value = 1;
+			if ( state.recording ) result.value = 3;
+			if ( state.queued ) result.value = 2;
+			controller.setClipLauncherLed( result );
 		};
-
-		var ca1 = [ 0, 0, 0, 0, 0, 0, 0, 0 ];
-		slots.addHasContentObserver( function( slot, has_content ) {
-			ca1[slot] = has_content ? 3 : 0;
-			controller.setClipLauncherLed( _led( slot, ca1[slot] ) );
+		slots.addHasContentObserver( function( slot, status ) {
+			_led( slot, "content", status );
 		});
-
-		var ca2 = [ 0, 0, 0, 0, 0, 0, 0, 0 ];
-		slots.addIsPlayingObserver( function( slot, is_playing ) {
-			ca2[slot] = is_playing ? 1 : ca1[slot];
-			controller.setClipLauncherLed( _led( slot, ca2[slot] ) );
+		slots.addIsPlayingObserver( function( slot, status ) {
+			_led( slot, "playing", status );
 		});
-
-		var ca3 = [ 0, 0, 0, 0, 0, 0, 0, 0 ];
-		slots.addIsQueuedObserver( function( slot, is_queued ) {
-			ca3[slot] = is_queued ? 6 : ca2[slot];
-			controller.setClipLauncherLed( _led( slot, ca3[slot] ) );
+		slots.addIsQueuedObserver( function( slot, status ) {
+			_led( slot, "queued", status );
+		});
+		slots.addIsRecordingObserver( function( slot, status ) {
+			_led( slot, "recording", status );
 		});
 	})( i, tracks.getTrack(i).getClipLauncherSlots() );
 
@@ -192,7 +223,6 @@ function init() {
 	// -------------------------------------------------------------------
 
 	controller.setEventCallback( "bpm_tap", function( value ) {
-		println( "current: " + value );
 		transport.getTempo().set( value - 20, 647 );
 	});
 
@@ -203,13 +233,14 @@ function init() {
 
 function exit() {
 	// TODO: Clean up mess after Bitwig
+	for ( var i = 0; i < 8; i += 1 ) (function( i, slots ) {
+		slots.setIndication( false );
+	})( i, tracks.getTrack(i).getClipLauncherSlots() );
 	controller.mode(0).stop();
-	host.showPopupNotification("APC40 plugged out");
 }
 
 function mapUserCtl( ctl, label, fun_on, fun_off ) {
 	ctl.setLabel( label );
-	ctl.setIndication( false );
 	if ( fun_on !== undefined ) ctl.addValueObserver( 128, fun_on );
 	if ( fun_off !== undefined ) ctl.addNameObserver( 2, "_u", function ( name ) {
 		if ( name === "_u" ) fun_off();
